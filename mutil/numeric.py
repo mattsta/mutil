ce50876ea1bfc461c5eda6110faa4a116b3611b6
Matt Stancliff <matt@genges.com>
2024-08-04 11:33:04 -0700

    Refactor rounding for performance and accuracy
    Fixes:
      - It was wrong to create Decimal() instances from actual floats. You
        have to create Decimal() input from strings so the decimal values
        are exactly what you expect.
      - So now if input is NOT a Decimal, we convert via Decimal(str(x))
      - Fixing Decimal creation also improves rounding accuracy with fewer
        hacks.
      - Replaced boolean parameter roundUp with a 3-way enum.
      - Now we can also round to nearest instead of just static up or down
        as well.
      - Refactored tests for new rounding input/output parameters types.
      - Added additional tests for additional rounding mode.

--- mutil/numeric.py
+++ mutil/numeric.py
@@ -1,10 +1,18 @@
-""" Numeric manipulation and printing """
+"""Numeric manipulation and printing"""
 
 from decimal import Decimal
+from enum import Enum
+
+ROUND = Enum("ROUND", "UP DOWN NEAR")
 
 
-def roundnear(end: float, price: float, roundUp: bool) -> float:
-    """Round a float to a multiple of 'end'.
+def roundnear(
+    increment: Decimal | float,
+    price: Decimal | float,
+    direction: ROUND = ROUND.NEAR,
+    decimals: int = 2,
+) -> Decimal:
+    """Round a float to a multiple of 'increment'.
 
     This is a more adjustable version of just:
         # example for enforcing 0.25 increments:
@@ -13,43 +21,60 @@ def roundnear(end: float, price: float, roundUp: bool) -> float:
     Note: 0.00 is a valid multiple if rounding down from near-zero or rounding
           up from a negative near-zero."""
 
-    # if price is ALREADY on an increment, then it already complies
-    # (this doesn't catch _all_ cases though...)
-    # Borrowed from https://stackoverflow.com/questions/13106953/python-round-05up
-    p = Decimal(price)
-    dend = Decimal(end)
-    remainder = p.remainder_near(dend)
+    if not isinstance(price, Decimal):
+        price = Decimal(str(price))
 
-    # already ends in the target round increment
-    # the 'remainder_near' can return negative "nearness" so account for it; also
-    # sometimes even with exact matches like 0.15 % 0.05, it returns 1.3e-17 (but
-    # using Decimal() is supposed to fix all those bad floating point math things
-    # anyway?), so also check a very tiny value to count as an exact match too.
-    if remainder == 0 or abs(remainder) < 0.0000000001:
+    if not isinstance(increment, Decimal):
+        increment = Decimal(str(increment))
+
+    # price already ends in the target round increment, so input price already works;
+    # just stop and return original (*NOW Decimal*) price here.
+    rem: float = price.remainder_near(increment)
+    if rem == 0:
         return price
 
-    # or maybe we want round up, so round to nearest upper breach of increment
-    if roundUp:
-        p = Decimal(price + (end / 2))
-    else:
-        # else, smaller
-        p = Decimal(price - (end / 2))
+    # else, we need to do some rounding to comply with increment.
+    # NOTE: for now, python evaluates 'match' directives sequentially
+    #       (like one big "if/else" statement), so the higher the case check
+    #       in a match statement, the faster is is reached.
+    #       From benchmarking, each additional un-matched 'case' hop is an additional
+    #       20 ns of processing in the Python 3.12 VM (but it was 70 ns per case hop in Python 3.10!).
+    #       Technically, just using if/else branches are 2 ns faster per case, but this still looks cleaner,
+    #       so I'll accept the additional 2 ns to 6 ns per match statement penalty for readability.
+    match direction:
+        case ROUND.NEAR:
+            # Decimal().remainder_near() returns:
+            #  - NEGATIVE VALUES when the CLOSEST next value is HIGHER
+            #  - POSITIVE VALUES when the CLOSEST next value is LOWER
+            if rem < 0:
+                # lower remainder means we are closer to the next higher increment,
+                # so round UP
+                price = price + (increment / 2)
+            else:
+                # higher remainder means we are closer to the previous lower increment,
+                # so round DOWN
+                price = price - (increment / 2)
+        case ROUND.UP:
+            price = price + (increment / 2)
+        case ROUND.DOWN:
+            price = price - (increment / 2)
 
-    remainder = p.remainder_near(dend)
-    return round(float(p - remainder), 2)
+    # move rounded price to the increment modulus
+    finalRemainder = price.remainder_near(increment)
+    return round(price - finalRemainder, decimals)
 
 
-def roundnear5(price: float, roundUp: bool) -> float:
+def roundnear5(price: float, direction: ROUND) -> Decimal:
     """Round 'price' to end in a multiple of '0.05'"""
-    return roundnear(0.05, price, roundUp)
+    return roundnear(0.05, price, direction)
 
 
-def roundnear10(price: float, roundUp: bool) -> float:
+def roundnear10(price: float, direction: ROUND) -> Decimal:
     """Round 'price' to end in a multiple of '0.10'"""
-    return roundnear(0.10, price, roundUp)
+    return roundnear(0.10, price, direction)
 
 
-def fmtPrice(x, decimals=4):
+def fmtPrice(x, decimals=4) -> str:
     """Print price with minimum of two digit decimals or maximum
     of 4 decimals.
 
@@ -72,7 +97,7 @@ def fmtPrice(x, decimals=4):
     return f"{x:,.{finalLen}g}"
 
 
-def fmtPricePad(n, padding=10, decimals=4):
+def fmtPricePad(n, padding=10, decimals=4) -> str:
     """Format price with padding as:
     - two decimal precision if <= 2 decimals
         - i.e. print 120 as 120.00
@@ -96,7 +121,7 @@ def fmtPricePad(n, padding=10, decimals=4):
 
         return f"{' ':>{padding}}"
 
-    # print at most 4 digits, not ending in zeroes...
+    # print at most N digits, not ending in zeroes...
     dn = Decimal(str(round(n, decimals)))
     sign, digits, exponent = dn.as_tuple()
 
--- tests/test_numeric.py
+++ tests/test_numeric.py
@@ -1,4 +1,7 @@
 import mutil.numeric as n
+from decimal import Decimal
+
+from mutil.numeric import ROUND
 
 
 def test_noround():
@@ -8,36 +11,36 @@ def test_noround():
     # on a matching round condition still caused the rounding
     # logic to change the input value (which was very bad!)
     for p in (0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35):
-        assert n.roundnear(0.05, p, True) == p
-        assert n.roundnear(0.05, p, False) == p
+        assert n.roundnear(0.05, p, ROUND.UP) == Decimal(str(Decimal(str(p))))
+        assert n.roundnear(0.05, p, ROUND.DOWN) == Decimal(str(Decimal(str(p))))
 
 
 def test_rn5_up():
     """Round up"""
-    assert n.roundnear5(0.24, True) == 0.25
-    assert n.roundnear5(0.01, True) == 0.05
-    assert n.roundnear5(-0.01, True) == 0.00
+    assert n.roundnear5(0.24, ROUND.UP) == Decimal(str(0.25))
+    assert n.roundnear5(0.01, ROUND.UP) == Decimal(str(0.05))
+    assert n.roundnear5(-0.01, ROUND.UP) == Decimal(str(0.00))
 
 
 def test_rn5_down():
     """Round down"""
-    assert n.roundnear5(0.24, False) == 0.20
-    assert n.roundnear5(0.01, False) == 0.00
-    assert n.roundnear5(-0.01, False) == -0.05
+    assert n.roundnear5(0.24, ROUND.DOWN) == Decimal(str(0.20))
+    assert n.roundnear5(0.01, ROUND.DOWN) == Decimal(str(0.00))
+    assert n.roundnear5(-0.01, ROUND.DOWN) == Decimal(str(-0.05))
 
 
 def test_rn10_up():
     """Round up"""
-    assert n.roundnear10(0.24, True) == 0.30
-    assert n.roundnear10(0.01, True) == 0.10
-    assert n.roundnear10(-0.01, True) == 0.00
+    assert n.roundnear10(0.24, ROUND.UP) == Decimal(str(0.30))
+    assert n.roundnear10(0.01, ROUND.UP) == Decimal(str(0.10))
+    assert n.roundnear10(-0.01, ROUND.UP) == Decimal(str(0.00))
 
 
 def test_rn10_down():
     """Round down"""
-    assert n.roundnear10(0.24, False) == 0.20
-    assert n.roundnear10(0.01, False) == 0.00
-    assert n.roundnear10(-0.01, False) == -0.10
+    assert n.roundnear10(0.24, ROUND.DOWN) == Decimal(str(0.20))
+    assert n.roundnear10(0.01, ROUND.DOWN) == Decimal(str(0.00))
+    assert n.roundnear10(-0.01, ROUND.DOWN) == Decimal(str(-0.10))
 
 
 def roundnear7(p, updown):
@@ -45,18 +48,60 @@ def roundnear7(p, updown):
     return n.roundnear(0.07, p, updown)
 
 
+def test_roundnear025():
+    assert n.roundnear(0.025, 1.025, ROUND.UP, 3) == Decimal(str(1.025))
+    assert n.roundnear(0.025, 1.026, ROUND.UP, 3) == Decimal(str(1.050))
+    assert n.roundnear(0.025, 1.026, ROUND.DOWN, 3) == Decimal(str(1.025))
+
+
+def test_roundnear025_NEAR():
+    assert n.roundnear(0.025, 1.025, ROUND.NEAR, 3) == Decimal(str(1.025))
+    assert n.roundnear(0.025, 1.026, ROUND.NEAR, 3) == Decimal(str(1.025))
+    assert n.roundnear(0.025, 1.027, ROUND.NEAR, 3) == Decimal(str(1.025))
+    assert n.roundnear(0.025, 1.028, ROUND.NEAR, 3) == Decimal(str(1.025))
+    assert n.roundnear(0.025, 1.029, ROUND.NEAR, 3) == Decimal(str(1.025))
+    assert n.roundnear(0.025, 1.030, ROUND.NEAR, 3) == Decimal(str(1.025))
+    assert n.roundnear(0.025, 1.031, ROUND.NEAR, 3) == Decimal(str(1.025))
+    assert n.roundnear(0.025, 1.032, ROUND.NEAR, 3) == Decimal(str(1.025))
+    assert n.roundnear(0.025, 1.033, ROUND.NEAR, 3) == Decimal(str(1.025))
+    assert n.roundnear(0.025, 1.034, ROUND.NEAR, 3) == Decimal(str(1.025))
+    assert n.roundnear(0.025, 1.035, ROUND.NEAR, 3) == Decimal(str(1.025))
+    assert n.roundnear(0.025, 1.036, ROUND.NEAR, 3) == Decimal(str(1.025))
+    assert n.roundnear(0.025, 1.037, ROUND.NEAR, 3) == Decimal(str(1.025))
+    # switchover between rounding down and rounding up
+    assert n.roundnear(0.025, 1.038, ROUND.NEAR, 3) == Decimal(str(1.050))
+    assert n.roundnear(0.025, 1.039, ROUND.NEAR, 3) == Decimal(str(1.050))
+    assert n.roundnear(0.025, 1.040, ROUND.NEAR, 3) == Decimal(str(1.050))
+    assert n.roundnear(0.025, 1.041, ROUND.NEAR, 3) == Decimal(str(1.050))
+    assert n.roundnear(0.025, 1.042, ROUND.NEAR, 3) == Decimal(str(1.050))
+    assert n.roundnear(0.025, 1.043, ROUND.NEAR, 3) == Decimal(str(1.050))
+    assert n.roundnear(0.025, 1.044, ROUND.NEAR, 3) == Decimal(str(1.050))
+    assert n.roundnear(0.025, 1.045, ROUND.NEAR, 3) == Decimal(str(1.050))
+    assert n.roundnear(0.025, 1.046, ROUND.NEAR, 3) == Decimal(str(1.050))
+    assert n.roundnear(0.025, 1.047, ROUND.NEAR, 3) == Decimal(str(1.050))
+    assert n.roundnear(0.025, 1.048, ROUND.NEAR, 3) == Decimal(str(1.050))
+    assert n.roundnear(0.025, 1.049, ROUND.NEAR, 3) == Decimal(str(1.050))
+    assert n.roundnear(0.025, 1.050, ROUND.NEAR, 3) == Decimal(str(1.050))
+
+
+def test_roundnear025_under():
+    assert n.roundnear(0.025, -1.025, ROUND.UP, 3) == Decimal(str(-1.025))
+    assert n.roundnear(0.025, -1.026, ROUND.UP, 3) == Decimal(str(-1.025))
+    assert n.roundnear(0.025, -1.026, ROUND.DOWN, 3) == Decimal(str(-1.050))
+
+
 def test_rn7_up():
     """Round up"""
-    assert roundnear7(0.24, True) == 0.28
-    assert roundnear7(0.01, True) == 0.07
-    assert roundnear7(-0.01, True) == 0.00
+    assert roundnear7(0.24, ROUND.UP) == Decimal(str(0.28))
+    assert roundnear7(0.01, ROUND.UP) == Decimal(str(0.07))
+    assert roundnear7(-0.01, ROUND.UP) == Decimal(str(0.00))
 
 
 def test_rn7_down():
     """Round down"""
-    assert roundnear7(0.24, False) == 0.21
-    assert roundnear7(0.01, False) == 0.00
-    assert roundnear7(-0.01, False) == -0.07
+    assert roundnear7(0.24, ROUND.DOWN) == Decimal(str(0.21))
+    assert roundnear7(0.01, ROUND.DOWN) == Decimal(str(0.00))
+    assert roundnear7(-0.01, ROUND.DOWN) == Decimal(str(-0.07))
 
 
 def roundnear1xx(p, updown):
@@ -66,16 +111,16 @@ def roundnear1xx(p, updown):
 
 def test_rn1xx_up():
     """Round up"""
-    assert roundnear1xx(0.24, True) == 1
-    assert roundnear1xx(700.23, True) == 701
-    assert roundnear1xx(-0.01, True) == 0.00
+    assert roundnear1xx(0.24, ROUND.UP) == Decimal(str(1))
+    assert roundnear1xx(700.23, ROUND.UP) == Decimal(str(701))
+    assert roundnear1xx(-0.01, ROUND.UP) == Decimal(str(0.00))
 
 
 def test_rn1xx_down():
     """Round down"""
-    assert roundnear1xx(0.24, False) == 0.00
-    assert roundnear1xx(700.23, False) == 700
-    assert roundnear1xx(-0.01, False) == -1
+    assert roundnear1xx(0.24, ROUND.DOWN) == Decimal(str(0.00))
+    assert roundnear1xx(700.23, ROUND.DOWN) == Decimal(str(700))
+    assert roundnear1xx(-0.01, ROUND.DOWN) == Decimal(str(-1))
 
 
 def test_fmtBasic():
@@ -97,5 +142,5 @@ def test_fmtBasic():
         assert n.fmtPrice(a) == b
 
         # test negative versions too
-        assert n.fmtPricePad(-a, 0) == "-" + b
-        assert n.fmtPrice(-a) == "-" + b
+        assert n.fmtPricePad(-a, 0) == ("-" + b)
+        assert n.fmtPrice(-a) == ("-" + b)
